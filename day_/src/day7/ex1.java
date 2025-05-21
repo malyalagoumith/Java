c5d71811bc2c0c4db7351fa3be8b1b3025902f74
malyalagoumith <malyalagoumith1@gmail.com>
2025-05-21 16:46:15 +0530

    Update ex1.java

--- day_/src/day7/ex1.java
+++ day_/src/day7/ex1.java
@@ -1,7 +1,2 @@
-import java.util.*;
-package day7;
-public class ex1{
-	public static void main(String[] args) throws ArithmeticException  {
-		Fiile Obj=new File()
-		Scanner obj = new Scanner(System.in);
+
 		
